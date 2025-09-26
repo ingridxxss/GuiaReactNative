17ef1c64230a30ed5496e5d279e34c9af27926d2
Ingrid <sensioncaballero@escuelasproa.edu.ar>
2025-09-25 23:26:29 -0300

    comit finalll

--- guiareactNative/Componentes/Actividad1.js
+++ guiareactNative/Componentes/Actividad1.js
@@ -13,7 +13,7 @@ function Actividad1(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00d5ffff',
+    backgroundColor: '#BA68C8 ',
     alignItems: 'center',
     justifyContent: 'center',
   },
